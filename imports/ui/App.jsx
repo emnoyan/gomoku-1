ee6ebb4e12aef7d1ad6755f66e8078033bf707eb
hienpham <hien.pham@axonactive.vn>
2017-05-11 17:39:07 +0700

    create board array

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -6,6 +6,8 @@ import { Selections } from '../api/selections.js';
 import Task from './Task.jsx';
  
 // App component - represents the whole app
+
+const BOARD_SIZE = 15
 class App extends Component {
  
   handleSubmit(event) {
@@ -24,7 +26,11 @@ class App extends Component {
   }
 
   handleCellClick(row, col) {
-
+    Selections.insert({
+      type: 'circle',
+      row,
+      col
+    });
   }
 
   renderTasks() {
@@ -38,7 +44,8 @@ class App extends Component {
       <div className="boardRow">
       {[...Array(15).keys()].map(col => (
         <div className="boardCol">
-          <div className="boardCell" id={row + "-" + col} onClick={() => this.handleCellClick(row, col)}>
+          <div className={"boardCell" + (this.props.board[row][col] === 'circle'? ' boardCellCircle' : '')} id={row + "-" + col} key={row + "-" + col} onClick={() => this.handleCellClick(row, col)}>
+
           </div>
         </div>
         ))}
@@ -48,23 +55,18 @@ class App extends Component {
   render() {
     return (
       <div className="container">
-        <header>
-          <h1>Todo List</h1>
-          <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
-            <input
-              type="text"
-              ref="textInput"
-              placeholder="Type to add new tasks"
-            />
-            <button type="submit" value="Add">Add</button>
-          </form>
-        </header>
- 
-        <ul>
-          {this.renderTasks()}
-        </ul>
+        <div className="controls">
+          <button className="btn">
+              New
+          </button>
+          <div className="messages">
+            <div className="messagesContainer">
+              <div className="newMainText" id="message">try to get 5 in a row!</div>
+            </div>
+          </div>
+        </div>
         <div className="board">
-        {this.renderBoard()}
+            {this.renderBoard()}
         </div>
       </div>
     );
@@ -72,12 +74,22 @@ class App extends Component {
 }
 
 App.propTypes = {
-  tasks: PropTypes.array.isRequired,
+  board: PropTypes.array.isRequired,
 };
 
 export default createContainer(() => {
+  let board = new Array(15)
+  for(let i=0; i< 15; i++) {
+    board[i] = new Array(15)
+  }
+  const selection = Selections.find({ type : 'circle'}).fetch()
+  board = selection.reduce((pre, item) => {
+    pre[item.row][item.col] = 'circle'
+    return pre
+  }, board)
+
   return {
     tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
-    circle : Selections.find({ type : 'circle'}).map(item => a[item.x][item.y]).fetch()
-  };
-}, App);
+    board
+  }
+}, App)
